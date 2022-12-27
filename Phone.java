3967b7a7fe5fa9850cebc9e34a638b975f61e5f2
Lenar <lenar.shamsiyarov.04@gmail.com>
2022-12-27 23:10:21 +0500

    Create Phone.java

---
+++ Phone.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Phone {
+    int number;
+    String model;
+    double weight;
+
+    public static void receiveCall(){
+        String name = "Lenar";
+        System.out.print(name+" call to you");
+    }
+    public static void getNumber(){
+        int number = 8927304;
+    }
+}
